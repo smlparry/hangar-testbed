61af6b9387054607ad7275079f32fe623021ffdc
smlparry <samuel@zaprri.com>
2026-07-26 03:52:34 -0700

    Add bulk pricing tiers

--- cart.js
+++ cart.js
@@ -11,4 +11,13 @@ function applyDiscount(total, percent) {
   return total - (total * percent / 100);
 }
 
-module.exports = { subtotal, applyDiscount };
+// Bulk pricing: 10% off at 10 units, 25% off at 50.
+function bulkDiscount(items) {
+  const units = items.reduce((n, i) => n + i.qty, 0);
+  let percent = 0;
+  if (units > 10) percent = 10;
+  if (units > 50) percent = 25;
+  return applyDiscount(subtotal(items), percent);
+}
+
+module.exports = { subtotal, applyDiscount, bulkDiscount };
